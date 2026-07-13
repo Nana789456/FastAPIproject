6e92bdcb916db1d864ef0986d51b7f0b363789d7
VladislavSoren <vlad224vlgu@gmail.com>
2026-07-13 08:49:03 +0300

    Добавить методы VideoRepository.get_by_title и list_last

--- app/repositories.py
+++ app/repositories.py
@@ -32,16 +32,14 @@ class VideoRepository:
         self.session.add(video)
         return video
 
-"""
-    def get_by_name(self, course_name: str) -> Course | None:
-        stmt = select(Course).where(Course.name == course_name)
+    def get_by_title(self, title: str) -> Video | None:
+        stmt = select(Video).where(Video.title == title)
         return self.session.execute(stmt).scalar_one_or_none()
 
-    def list_last(self, number: int) -> list[Course]:
+    def list_last(self, number: int) -> list[Video]:
         return (
-            self.session.query(Course)
-            .order_by(Course.name)
+            self.session.query(Video)
+            .order_by(Video.title)
             .limit(number)
             .all()
         )
-"""
